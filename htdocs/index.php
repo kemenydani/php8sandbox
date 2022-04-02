2c9642e51126cf2a2023b0a08ff5b846f84a5ca0
daniel.kemeny <daniel.kemeny@check24.de>
2022-04-02 12:20:16 +0200

    Improve project structure

--- htdocs/index.php
+++ htdocs/index.php
@@ -6,7 +6,7 @@ use src\framework\request\ControllerResolver;
 require_once '../vendor/autoload.php';
 
 const __PUBLIC__ = __DIR__;
-const __CONFIG__ = __DIR__ . "/../src/config";
+const __CONFIG__ = __DIR__ . "/../src/application/config";
 
 (new ControllerResolver([
     ArticleViewController::class
